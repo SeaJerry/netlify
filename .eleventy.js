aecd473224f2c63037ef58a4407fa7cb12bcda10
SeaJerry <jsea@kindsys.us>
2022-07-08 16:55:07 -0700

    fix admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
   // Function to tell eleventy to copy the assets and css file to the public folder for display
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
