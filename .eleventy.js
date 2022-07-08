087c3646dcd97885235bec74fe10d4a6d7524f57
SeaJerry <jsea@kindsys.us>
2022-07-08 16:25:43 -0700

    Add featured pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,20 @@
 // config file
-module.exports = function(eleventyConfig) {
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    }
-}
+
+const { DateTime } = require("luxon");
+// Modify the date time on the blog post
+module.exports = function (eleventyConfig) {
+  // Function to tell eleventy to copy the assets and css file to the public folder for display
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  })
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
